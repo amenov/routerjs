db10020dec37c82ed316004f2ba749bf6ad165ed
Абдулсалам Аменов <amenov.abdulsalam@gmail.com>
2020-10-09 17:36:21 +0600

    A 1736 09102020

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = (router, routes, paths) => {
+  const parser = (arr, parentUrl = "", parentMiddleware = []) => {
+    arr.forEach((route) => {
+      const url = parentUrl + route.url;
+
+      const middleware = [...parentMiddleware];
+
+      if (route?.middleware?.length) {
+        route.middleware.forEach((middlewarePath) =>
+          middleware.push(require(paths.middleware + middlewarePath))
+        );
+      }
+
+      if (route.method && route.controller) {
+        const method = route.method.toLowerCase();
+
+        const [controllerPath, controllerMethod] = route.controller.split(".");
+
+        const controller = require(paths.controller + controllerPath);
+
+        router[method](url, ...middleware, controller[controllerMethod]);
+      }
+
+      if (route?.children?.length) parser(route.children, url, middleware);
+    });
+  };
+
+  parser(routes);
+
+  return router;
+};
