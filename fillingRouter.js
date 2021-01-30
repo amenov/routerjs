18c495a6b24ad1f3dd375a6a9f81c2863785565b
Amenov <amenov.abdulsalam@gmail.com>
2021-01-30 12:16:16 +0600

    AM 1216 30012021

---
+++ fillingRouter.js
@@ -0,0 +1,61 @@
+const express = require('express');
+
+const Router = express.Router();
+
+const checkType = (val, Type) => val.__proto__ === Type.prototype;
+
+module.exports = (routes, paths) => {
+  const recursiveFilling = (routes, prevUrl = '', prevMiddleware = []) => {
+    for (const route of routes) {
+      prevUrl = prevUrl + (route.url ?? '');
+      prevMiddleware = [...prevMiddleware];
+
+      const pushPrevMiddleware = (middleware) => {
+        prevMiddleware.push(middleware);
+      };
+
+      const requireMiddleware = (path) => {
+        require(paths.middleware + '/' + path);
+      };
+
+      const routeMiddleware = route.middleware;
+
+      if (routeMiddleware) {
+        if (checkType(routeMiddleware, String)) {
+          pushPrevMiddleware(requireMiddleware(routeMiddleware));
+        } else if (checkType(routeMiddleware, Array)) {
+          for (const middleware of routeMiddleware) {
+            if (checkType(middleware, String)) {
+              pushPrevMiddleware(requireMiddleware(middleware));
+            } else if (checkType(middleware, Function)) {
+              pushPrevMiddleware(middleware);
+            }
+          }
+        }
+      }
+
+      if (route.method && route.controller) {
+        const method = route.method.toLowerCase();
+        const url = prevUrl;
+        const middleware = prevMiddleware;
+        const [controllerPath, controllerMethod] = route.controller.split('.');
+
+        const controller = require(paths.controllers + '/' + controllerPath);
+
+        Router[method](
+          url,
+          ...middleware,
+          controllerMethod ? controller[controllerMethod] : controller
+        );
+      }
+
+      if (route.children?.length) {
+        recursiveFilling(route.children, prevUrl, prevMiddleware);
+      }
+    }
+  };
+
+  recursiveFilling(routes);
+
+  return Router;
+};
--- index.js
+++ index.js
@@ -1,43 +1,7 @@
-const router = require('express').Router();
+const fillingRouter = require('./fillingRouter');
 
 module.exports = (routes, paths) => {
-  const parser = (routes, parentUrl = '', parentMiddleware = []) => {
-    routes.forEach((route) => {
-      const url = parentUrl + (route.url ? route.url : '');
+  const Router = fillingRouter(routes, paths);
 
-      const middleware = [...parentMiddleware];
-
-      if (route.middleware) {
-        if (!Array.isArray(route.middleware)) {
-          route.middleware = route.middleware.split();
-        }
-
-        route.middleware.forEach((middlewarePath) => {
-          middleware.push(require(paths.middleware + middlewarePath));
-        });
-      }
-
-      if (route.method && route.controller) {
-        const method = route.method.toLowerCase();
-
-        const [controllerPath, controllerMethod] = route.controller.split('.');
-
-        const controller = require(paths.controllers + controllerPath);
-
-        router[method](
-          url,
-          ...middleware,
-          controllerMethod ? controller[controllerMethod] : controller
-        );
-      }
-
-      if (route.children?.length) {
-        parser(route.children, url, middleware);
-      }
-    });
-  };
-
-  parser(routes);
-
-  return router;
+  return Router;
 };
